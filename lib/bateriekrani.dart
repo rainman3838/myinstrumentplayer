4f72e926e19e67f6eb66379a5556e56285065e7d
lkarauzl <karauzahmet@gmail.com>
2024-12-24 14:56:11 +0300

    bateri ekranin tasarimi tamamlanmaya yaklasito

--- lib/bateriekrani.dart
+++ lib/bateriekrani.dart
@@ -1,5 +1,4 @@
 import 'package:audioplayers/audioplayers.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class BateriEkrani extends StatelessWidget {
@@ -14,32 +13,33 @@ class BateriEkrani extends StatelessWidget {
 
 class DrumPadScreen extends StatelessWidget {
   final List<String> soundPaths = [
-    'sounds/kalimbases/1kalimbado.mp3',
-    'sounds/kalimbases/2kalimbare.mp3',
-    'sounds/kalimbases/1kalimbado.mp3',
-    'sounds/kalimbases/1kalimbado.mp3',
-    'sounds/kalimbases/1kalimbado.mp3',
-    'sounds/kalimbases/1kalimbado.mp3',
-    'sounds/kalimbases/1kalimbado.mp3',
-    'sounds/kalimbases/1kalimbado.mp3',
-    'sounds/kalimbases/1kalimbado.mp3',
-    'sounds/kalimbases/1kalimbado.mp3',
-    'sounds/kalimbases/1kalimbado.mp3',
-    'sounds/kalimbases/1kalimbado.mp3',
+    'sounds/drumses/a.wav',
+    'sounds/drumses/b.wav',
+    'sounds/drumses/c.wav',
+    'sounds/drumses/d.wav',
+    'sounds/drumses/e.wav',
+    'sounds/drumses/f.wav',
+    'sounds/drumses/g.wav',
+    'sounds/drumses/h.wav',
+    'sounds/drumses/i.wav',
+    'sounds/drumses/j.wav',
+    'sounds/drumses/k.wav',
+    'sounds/drumses/l.wav',
   ];
-  final List<Color> colors = [
-    Colors.red,
-    Colors.blue,
-    Colors.green,
-    Colors.orange,
-    Colors.purple,
-    Colors.yellow,
-    Colors.cyan,
-    Colors.pink,
-    Colors.indigo,
-    Colors.lime,
-    Colors.teal,
-    Colors.amber,
+
+  final List<List<Color>> gradientColors = [
+    [Colors.red, Colors.pink],
+    [Colors.pink, Colors.purple],
+    [Colors.purple, Colors.deepPurple],
+    [Colors.cyan, Colors.lightBlue],
+    [Colors.lightBlue, Colors.cyan],
+    [Colors.cyan, Colors.teal],
+    [Colors.green, Colors.lime],
+    [Colors.lime, Colors.yellow.shade600],
+    [Colors.yellow, Colors.orange],
+    [Colors.orange, Colors.deepOrange],
+    [Colors.deepOrange, Colors.redAccent],
+    [Colors.redAccent, Colors.red],
   ];
 
   final AudioPlayer audioPlayer = AudioPlayer();
@@ -47,44 +47,37 @@ class DrumPadScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black, // Arka plan siyah
       appBar: AppBar(
         title: Text('Drum Pad'),
         centerTitle: true,
         backgroundColor: Colors.deepPurple,
       ),
       body: Padding(
-        padding: const EdgeInsets.all(10.0),
+        padding: const EdgeInsets.all(16.0),
         child: GridView.builder(
           gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 3, // 3 sütun
-            crossAxisSpacing: 10,
-            mainAxisSpacing: 10,
+            crossAxisSpacing: 16,
+            mainAxisSpacing: 16,
           ),
-          itemCount: soundPaths.length, // 12 buton
+          itemCount: soundPaths.length, // 12 düğme
           itemBuilder: (context, index) {
-            return ElevatedButton(
-              onPressed: () async {
-                try {
-                  await audioPlayer.play(AssetSource(soundPaths[index]));
-                } catch (e) {
-                  print("Error playing sound: $e");
-                }
+            return GestureDetector(
+              onTap: () {
+                audioPlayer.play(AssetSource(soundPaths[index]));
               },
-              style: ElevatedButton.styleFrom(
+              child: Container(
+                decoration: BoxDecoration(
+                  gradient: RadialGradient(
+                    colors: gradientColors[index],
+                    center: Alignment.center,
+                    radius: 0.6,
+                  ),
+                  borderRadius: BorderRadius.circular(16),
 
-                backgroundColor: colors[index],
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
                 ),
 
-            ),
-              child: Text(
-                'Pad ${index + 1}',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                ),
               ),
             );
           },
